d9a5ed58f6ead1575bbc964e9ec272081f7af19a
saadnadeemsls@gmail.com <saadnadeemsls@gmail.com>
2023-12-06 23:20:10 +0500

    added insert search traverse

--- project/BTree.cpp
+++ project/BTree.cpp
@@ -1,18 +1,195 @@
 #include "BTree.h"
 
-BTree::BTree(int degree)
-{
 
+BTreeNode::BTreeNode(int T, bool isLeaf /*Data* d*/)
+{
+    this->T = T;
+    this->isleaf = isLeaf;
+    this->keys = new int[2 * T - 1];
+    this->Childptr = new BTreeNode * [2 * T];
+    this->numkeys = 0;
+    //this->data = nullptr;
+    //this->data = d;
 }
-void BTree::PrintTree()
-{
 
+BTreeNode* BTreeNode::search(int key)
+{
+    int index = 0;
+    for (; index<numkeys && key>keys[index]; index++)
+    {
+
+    }
+
+    if (this->isleaf && !(index < numkeys && key == keys[index]))
+    {
+        cout << "key not found" << endl;
+        return NULL;
+    }
+
+    if (index < numkeys && key == keys[index])
+    {
+        cout << "Key " << key << " found in the B-tree." << std::endl;
+        return this;
+    }
+
+    return Childptr[index]->search(key);
 }
-void BTree::insert(string)
-{
 
+void BTreeNode::split(int index, BTreeNode* splitee)
+{
+    BTreeNode* New = new BTreeNode(splitee->T, splitee->isleaf);
+    New->numkeys = T - 1;
+
+    for (int i = 0; i < T - 1; i++)
+    {
+        int jmp = T + i;
+        New->keys[i] = splitee->keys[jmp];
+    }
+
+    if (splitee->isleaf == false)
+    {
+        for (int i = 0; i < T; i++)
+        {
+            int jmp = T + i;
+            New->Childptr[i] = splitee->Childptr[jmp];
+        }
+    }
+
+    splitee->numkeys = T - 1;
+
+    int last = numkeys;
+    while (last > index)
+    {
+        Childptr[last + 1] = Childptr[last];
+        last--;
+    }
+    Childptr[index + 1] = New;
+
+    for (int j = numkeys - 1; j >= index; j--)
+    {
+        keys[j + 1] = keys[j];
+    }
+
+
+    keys[index] = splitee->keys[T - 1];
+
+    numkeys++;
 }
-void BTree::deletenode(string)
-{
 
+void BTreeNode::insert2(int key)
+{
+    int index = numkeys - 1;
+
+    if (isleaf == true)
+    {
+        while (index >= 0 && key < keys[index])
+        {
+            keys[index + 1] = keys[index];
+            index--;
+        }
+
+        keys[index + 1] = key;
+        numkeys++;
+    }
+    else
+    {
+        while (index >= 0 && key < keys[index])
+        {
+            index--;
+        }
+
+
+        if (Childptr[index + 1]->numkeys == 2 * T - 1)
+        {
+
+            split(index + 1, Childptr[index + 1]);
+
+
+            if (key > keys[index + 1])
+            {
+                index++;
+            }
+
+
+        }
+        Childptr[index + 1]->insert2(key);
+
+    }
+}
+
+void BTreeNode::traverse()
+{
+    int i;
+    for (i = 0; i < numkeys; i++) {
+        if (isleaf == false)
+            Childptr[i]->traverse();
+        cout << " " << keys[i];
+    }
+
+    if (isleaf == false)
+        Childptr[i]->traverse();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+BTree::BTree(int m)
+{
+    root = nullptr;
+    this->m = m;
+    t = (m / 2) + ((m % 2) != 0);
+}
+
+BTreeNode* BTree::search(int key)
+{
+    if (root != nullptr)
+    {
+        return root->search(key);
+    }
+    else
+    {
+        cout << "B-tree is empty." << std::endl;
+    }
+}
+
+void BTree::insert(int key)
+{
+    if (root != nullptr)
+    {
+        if (root->numkeys == 2 * t - 1)
+        {
+            BTreeNode* tmp = new BTreeNode(t, 0);
+            tmp->Childptr[0] = root;
+            tmp->split(0, root);
+
+            int index = 0;
+            if (tmp->keys[0] < key)
+            {
+                index++;
+            }
+            tmp->Childptr[index]->insert2(key);
+
+            root = tmp;
+        }
+        else
+        {
+            root->insert2(key);
+        }
+    }
+    else if (root == nullptr)
+    {
+        root = new BTreeNode(t, 1);
+        root->keys[0] = key;
+        root->numkeys = 1;
+    }
 }
--- project/BTree.h
+++ project/BTree.h
@@ -5,24 +5,36 @@ class BTreeNode
 public:
 	int* keys;
 	int T;
-	Data* data;
+	//Data* data;
 
 	BTreeNode** Childptr;
-	int numkeys; 
-	bool isleaf; 
+	int numkeys;
+	bool isleaf;
 
-	void search();
+	BTreeNode(int T, bool isLeaf/*, Data* d*/);
+	BTreeNode* search(int key);
+
+	void split(int index, BTreeNode* splitee);
+	void insert2(int key);
+	void traverse();
 };
 class BTree
 {
 public:
 	BTreeNode* root;
-	int degree;
+	int m;					// order
+	int t;					// ceil(m/2)->degree
 
-	BTree(int degree);
-	void PrintTree();
-	void insert(string);
-	void deletenode(string);
+	BTree(int m);
+	BTreeNode* search(int key);
+	void insert(int key);
 
+	void traverse() {
+		if (root != NULL)
+			root->traverse();
+	}
 };
+
+
+
 
